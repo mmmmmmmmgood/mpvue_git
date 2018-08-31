86a64aa61c346e2b9e3cd9424554ff1fe721ab7e
mmmmmmmmgood <2297675726@qq.com>
2018-08-31 16:16:36 +0800

    1

---
+++ src/components/todolist.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{myname}}</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      myname: 'maozipeng1'
+    }
+  }
+}
+</script>
+<style>
+    div{
+        color:red;
+    }
+</style>
+
---
+++ src/pages/todolists/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>hello world</div>
+        <todolist></todolist>
+    </div>
+</template>
+
+<script>
+import todolist from '@/components/todolist'
+export default {
+  components: {
+    todolist
+  }
+}
+</script>
+  
+
+
+<style>
+
+</style>
